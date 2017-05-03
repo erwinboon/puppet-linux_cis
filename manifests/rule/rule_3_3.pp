a61713c78c418f8b0ca83c754100931297876164
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2017-05-03 14:41:00 +0200

    Added ability to disable the disabling of ipv6 (work-around for a bug in centos6

--- manifests/rule/rule_3_3.pp
+++ manifests/rule/rule_3_3.pp
@@ -1,45 +1,53 @@
-class cis_rhel7::rule::rule_3_3 {
+class cis_rhel7::rule::rule_3_3 (
+  $disable_ipv6_router_advertisements = true,
+  $disable_ipv6_redirects = true,
+  $disable_ipv6 = true
+) {
 
-# includes Rules:
-#3.3.1 Ensure IPv6 router advertisements are not accepted 
-#3.3.2 Ensure IPv6 redirects are not accepted
-#3.3.3 Ensure IPv6 is disabled
+  # includes Rules:
+  #3.3.1 Ensure IPv6 router advertisements are not accepted 
+  #3.3.2 Ensure IPv6 redirects are not accepted
+  #3.3.3 Ensure IPv6 is disabled
 
-$file = '/etc/sysctl.conf'
+  $file = '/etc/sysctl.conf'
+  $fileCIS = '/etc/modprobe.d/CIS.conf'
 
-file_line { "(3.3.1) - ${file}: net.ipv6.conf.all.accept_ra=0":
-  ensure  => present,
-  path  => $file,
-  line  => 'net.ipv6.conf.all.accept_ra=0',
-  match => '^net.ipv6.conf.all.accept_ra',
-}
-file_line { "(3.3.1) - ${file}: net.ipv6.conf.default.accept_ra=0":
-  ensure  => present,
-  path  => $file,
-  line  => 'net.ipv6.conf.default.accept_ra=0',
-  match => '^net.ipv6.conf.default.accept_ra',
-}
+  if $disable_ipv6_router_advertisements {
+    file_line { "(3.3.1) - ${file}: net.ipv6.conf.all.accept_ra=0":
+      ensure  => present,
+      path  => $file,
+      line  => 'net.ipv6.conf.all.accept_ra=0',
+      match => '^net.ipv6.conf.all.accept_ra',
+    }
+    file_line { "(3.3.1) - ${file}: net.ipv6.conf.default.accept_ra=0":
+      ensure  => present,
+      path  => $file,
+      line  => 'net.ipv6.conf.default.accept_ra=0',
+      match => '^net.ipv6.conf.default.accept_ra',
+    }
+  }
 
-file_line { "(3.3.2) - ${file}: net.ipv6.conf.all.accept_redirects=0":
-  ensure  => present,
-  path  => $file,
-  line  => 'net.ipv6.conf.all.accept_redirects=0',
-  match => '^net.ipv6.conf.all.accept_redirects',
-}
-file_line { "(3.3.2) - ${file}: net.ipv6.conf.default.accept_redirect=0":
-  ensure  => present,
-  path  => $file,
-  line  => 'net.ipv6.conf.default.accept_redirects=0',
-  match => '^net.ipv6.conf.default.accept_redirects',
-}
+  if $disable_ipv6_redirects {
+    file_line { "(3.3.2) - ${file}: net.ipv6.conf.all.accept_redirects=0":
+      ensure  => present,
+      path  => $file,
+      line  => 'net.ipv6.conf.all.accept_redirects=0',
+      match => '^net.ipv6.conf.all.accept_redirects',
+    }
+    file_line { "(3.3.2) - ${file}: net.ipv6.conf.default.accept_redirect=0":
+      ensure  => present,
+      path  => $file,
+      line  => 'net.ipv6.conf.default.accept_redirects=0',
+      match => '^net.ipv6.conf.default.accept_redirects',
+    }
+  }
 
-$fileCIS = '/etc/modprobe.d/CIS.conf'
-
-file_line { "(3.3.3) ${fileCIS} - disable ipv6":
-  ensure  => present,
-  path    => $fileCIS,
-  line    => 'options ipv6 disable=1',
-}
-
+  if $disable_ipv6 {
+    file_line { "(3.3.3) ${fileCIS} - disable ipv6":
+      ensure  => present,
+      path    => $fileCIS,
+      line    => 'options ipv6 disable=1',
+    }
+  }
 } #EOF
 
